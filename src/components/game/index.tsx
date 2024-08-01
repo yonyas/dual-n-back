c431bea55bec5db2120a38afecb2bc0756a7d0ee
jiyonlee <jiyonlee.d@gmail.com>
2024-08-01 20:54:22 +0900

    fix: 클라이언트에서만 new Audio실행되도록 수정 - Audio 객체는 브라우저 환경에서만 사용할 수 있음

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -42,19 +42,22 @@ export default function Game() {
   } = useGameDataContext();
 
   const sounds: {
-    [key: number]: HTMLAudioElement;
+    [key: number]: HTMLAudioElement | undefined;
   } = useMemo(() => {
-    return {
-      1: new Audio("/sound/H.wav"),
-      2: new Audio("/sound/J.wav"),
-      3: new Audio("/sound/K.wav"),
-      4: new Audio("/sound/L.wav"),
-      5: new Audio("/sound/Q.wav"),
-      6: new Audio("/sound/R.wav"),
-      7: new Audio("/sound/S.wav"),
-      8: new Audio("/sound/A.wav"),
-      9: new Audio("/sound/G.wav"),
-    };
+    if (typeof window !== "undefined") {
+      return {
+        1: new Audio("/sound/H.wav"),
+        2: new Audio("/sound/J.wav"),
+        3: new Audio("/sound/K.wav"),
+        4: new Audio("/sound/L.wav"),
+        5: new Audio("/sound/Q.wav"),
+        6: new Audio("/sound/R.wav"),
+        7: new Audio("/sound/S.wav"),
+        8: new Audio("/sound/A.wav"),
+        9: new Audio("/sound/G.wav"),
+      };
+    }
+    return {};
   }, []);
 
   const trials = n == 2 ? 22 : 2 * n + 17;
@@ -118,7 +121,7 @@ export default function Game() {
         },
       ];
     });
-    sounds[randomSoundIndex].play();
+    sounds?.[randomSoundIndex]?.play();
   };
 
   const handleStop = () => {
