0dad3ce4a49ce07b4bd711ca812b5d139a70f77a
jiyonlee <jiyonlee.d@gmail.com>
2024-08-02 21:38:14 +0900

    refact: sounds util 분리

---
+++ src/utils/sounds.ts
@@ -0,0 +1,16 @@
+export const getSounds = () => {
+  if (typeof window !== "undefined") {
+    return {
+      1: new Audio("/sound/H.wav"),
+      2: new Audio("/sound/J.wav"),
+      3: new Audio("/sound/K.wav"),
+      4: new Audio("/sound/L.wav"),
+      5: new Audio("/sound/Q.wav"),
+      6: new Audio("/sound/R.wav"),
+      7: new Audio("/sound/P.wav"),
+      8: new Audio("/sound/A.wav"),
+      9: new Audio("/sound/G.wav"),
+    };
+  }
+  return {} as Record<number, HTMLAudioElement>;
+};
