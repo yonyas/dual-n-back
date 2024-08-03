fd4cfed1bc98bb1f663f73ef77afd3d2b4cfe9df
jiyonlee <jiyonlee.d@gmail.com>
2024-08-03 22:35:08 +0900

    fix: 첫 렌더링시 antd 적용안되는 현상 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import {
   UserOutlined,
 } from "@ant-design/icons";
 import { Image } from "antd";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,47 +24,49 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} ${s.body}`}>
-        <header className={s.header}>
-          <div className="logo">
-            <a href="#">
-              <Image
-                src={"/Logo high-res.png"}
-                height={60}
-                preview={false}
-                alt={"nback logo"}
-              />
+        <AntdRegistry>
+          <header className={s.header}>
+            <div className="logo">
+              <a href="#">
+                <Image
+                  src={"/Logo high-res.png"}
+                  height={60}
+                  preview={false}
+                  alt={"nback logo"}
+                />
+              </a>
+            </div>
+            {/* TODO: login 기능은 나중에 개발 */}
+            {/* <Button>Sign Up</Button> */}
+            {/* <Button>Log In</Button> */}
+          </header>
+
+          {children}
+
+          <footer className={s.footer}>
+            <a
+              href="https://github.com/yonyas/dual-n-back"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubOutlined />
             </a>
-          </div>
-          {/* TODO: login 기능은 나중에 개발 */}
-          {/* <Button>Sign Up</Button> */}
-          {/* <Button>Log In</Button> */}
-        </header>
-
-        {children}
-
-        <footer className={s.footer}>
-          <a
-            href="https://github.com/yonyas/dual-n-back"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GithubOutlined />
-          </a>
-          <a
-            href="https://yonyas.github.io"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <UserOutlined />
-          </a>
-          <a
-            href="https://www.instagram.com/mouseyon"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InstagramOutlined />
-          </a>
-        </footer>
+            <a
+              href="https://yonyas.github.io"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <UserOutlined />
+            </a>
+            <a
+              href="https://www.instagram.com/mouseyon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <InstagramOutlined />
+            </a>
+          </footer>
+        </AntdRegistry>
       </body>
     </html>
   );
