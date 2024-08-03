4d9cb219c77c495a2d0b61ad42399ec1b9a0cef4
jiyonlee <jiyonlee.d@gmail.com>
2024-08-03 21:49:54 +0900

    remove antd button effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Flex } from "antd";
+import { ConfigProvider, Flex } from "antd";
 import "./global.css";
 import Game from "@/components/game";
 import Result from "@/components/result";
@@ -9,18 +9,31 @@ import StimuliProvider from "@/context/stimuliContext";
 export default function Home() {
   return (
     <main style={{ margin: "20px", height: "100%" }}>
-      <StimuliProvider>
-        <GameControlProvider>
-          <Flex
-            justify="space-around"
-            align="center"
-            style={{ height: "100%" }}
-          >
-            <Game />
-            <Result />
-          </Flex>
-        </GameControlProvider>
-      </StimuliProvider>
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              lineWidthFocus: 0,
+            },
+          },
+          token: {
+            motion: false,
+          },
+        }}
+      >
+        <StimuliProvider>
+          <GameControlProvider>
+            <Flex
+              justify="space-around"
+              align="center"
+              style={{ height: "100%" }}
+            >
+              <Game />
+              <Result />
+            </Flex>
+          </GameControlProvider>
+        </StimuliProvider>
+      </ConfigProvider>
     </main>
   );
 }
